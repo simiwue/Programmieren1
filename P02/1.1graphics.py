5b8650742a04b2ad16026ec7b8d202d1600b79d6
simiwue <simiwue@gmail.com>
2023-10-07 11:54:36 +0200

    sternchen-Aufgabe updated

--- P02/1.1graphics.py
+++ P02/1.1graphics.py
@@ -1,17 +1,21 @@
-stars = ["*", "* *", "* * *", "* *", "*"]
-for s in stars:
-    print(s*str("* "))
+# ==== einfache Version ====
 
-# stars = "*"
-# max_stars = int(input("How many stars do you want? \n Enter an integer > 0")) - 1
+# stars = ["*", "* *", "* * *", "* *", "*"]
+# for s in stars:
+#     print(s*str("* "))
 
-# while len(stars) < (max_stars*2):
-#     print(stars)
-#     stars = stars + " *"
+# ===== erweiterte Version - Anzahl Sterne kann gewählt werden ======
+
+stars = "*"
+max_stars = int(input("How many stars do you want? \n Enter an integer > 0")) - 1
+
+while len(stars) < (max_stars*2):
+    print(stars)
+    stars = stars + " *"
 
 
-# else:
-#     while len(stars) > 0:
-#         print(stars)
-#         stars = stars[:-2]
+else:
+    while len(stars) > 0:
+        print(stars)
+        stars = stars[:-2]
 
