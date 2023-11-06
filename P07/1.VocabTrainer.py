d152a7803f790c7d020f6bdc810b5adf5c34e716
Simon Wüthrich <simiwue@gmail.com>
2023-11-06 20:32:34 +0100

    vocabTrainer - multiple Solutions & word once

--- P07/1.VocabTrainer.py
+++ P07/1.VocabTrainer.py
@@ -2,7 +2,7 @@
 import random
 # Dictionary mit allen Wörtern wird festgelegt
 vocab = {
-    'key': 'Schlüssel',
+    'key': ['Schlüssel', 'Taste'],
     'door': 'Türe',
     'lock': 'Schloss',
     'dog': 'Hund',
@@ -13,21 +13,34 @@ vocab = {
 en_words = list(vocab.keys())
 # de_words = vocab.values()
 
-cont = True
 
-# In der while-Schleife fiindet das Abfragen der Wörter statt. 
-while cont:
-
+# Frage Wörter, solange die Liste Wörter enthält (zusätzlich eingebaut; nicht in Aufgabe verlangt)
+while len(en_words)>0:
+    
     # Eine Zufallsezahl wird gewählt
-    rand_num = random.randint(0, len(en_words))
+    rand_num = random.randint(0, len(en_words)-1)
     # Damit wird ein Wort aus der Liste ausgewählt.
     word = en_words[rand_num]
-    # User-INput wird als answer gespeichert.
+    # User-Input wird als answer gespeichert.
     answer = input("Uebersetze: " + word)
     # Es wird kontrolliert, ob die Antwort richtig war.
-    if answer == vocab[word]:
+    if answer in vocab[word]:
         print("Deine Antwort war richtig!")
+        # löscht das Wort aus der Liste, damit keine Wiederholung auftritt
+        en_words.remove(word)
 
     else:
-        print("Falsch \nrichtige Antwort wäre " + vocab[word])
+        # Kontrolliere, ob die Lösung eine Liste ist oder ein einzelnes Wort
+        if isinstance(vocab[word], list):
+            # Wenn es eine Liste ist, verbinde die einzelnen Lösungen mit " oder " zu einem schönen String
+            solution = ' oder '.join(vocab[word])
+
+        else:
+            solution = str(vocab[word])
+            
+        print("Falsch! \nRichtige Antwort wäre: " + solution)
+
+
+# Wird getriggert, wenn alle Wörter richtig beantwortet wurden und keine Wörter mehr in der Liste vorhanden sind
+print("Glückwunsch du hast alle Wörter gewusst!")
 
