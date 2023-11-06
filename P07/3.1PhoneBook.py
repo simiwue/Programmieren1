6d6f1d1555822a7921d137fac02022ccdad0d3a0
Simon Wüthrich <simiwue@gmail.com>
2023-11-06 21:59:34 +0100

    phoneBook_basic - done

---
+++ P07/3.1PhoneBook.py
@@ -0,0 +1,75 @@
+import json
+
+def print_numbers(numbers):
+    #print all numbers
+    for number in numbers.values():
+        print(number)
+
+def add_number(numbers, name, number):
+    numbers.update({name: number})
+
+def remove_number(numbers, name):
+    numbers.pop(name)
+
+def lookup_number(numbers, name):
+    print(numbers[name])
+
+def load_numbers(numbers, filename):
+    json_file = open('P07/' + filename)
+    new_numbers = json.load(json_file)
+    numbers.update(new_numbers)
+    print(numbers)
+
+def save_numbers(numbers, filename):
+    json_object = json.dumps(numbers)
+    with open('P07/' + filename, 'w') as outfile:
+        outfile.write(json_object)
+
+def print_menu():
+    print ('1. Print all Phone Numbers')
+    print ('2. Add a Phone Number')
+    print ('3. Remove a Phone Number')
+    print ('4. Lookup a Phone Number')
+    print ('5. Load numbers')
+    print ('6. Save numbers')
+    print ('7. Quit')
+    print()
+phone_book = {}
+menu_choice = 0
+print_menu()
+
+
+
+while True:
+    menu_choice = int(input("Type in a number (1-7): "))
+    if menu_choice == 1:
+        print_numbers(phone_book)
+
+    elif menu_choice == 2:
+        print("Add Name and Number")
+        name = input("Name: ")
+        phone = input("Number: ")
+        add_number(phone_book, name, phone)
+
+    elif menu_choice == 3:
+        print("Remove Name and Number")
+        name = input("Name: ")
+        remove_number(phone_book, name)
+
+    elif menu_choice == 4:
+        print("Lookup Number")
+        name = input("Name: ")
+        print(lookup_number(phone_book, name))
+
+    elif menu_choice == 5:
+        filename = input("Filename to load: ")
+        load_numbers(phone_book, filename)
+
+    elif menu_choice == 6:
+        filename = input("Filename to save: ")
+        save_numbers(phone_book, filename)
+
+    elif menu_choice == 7:
+        break
+    else:
+        print_menu()
